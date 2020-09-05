2a426f06be7c4054f4dda0a4ebfc33156abeca85
jacky_gy_zhu <jacky.gy.zhu@gmail.com>
2020-09-05 17:17:32 +1000

    .

--- src/main/java/com/skillone/designpatterns/adapter/classadapter/Client.java
+++ src/main/java/com/skillone/designpatterns/adapter/classadapter/Client.java
@@ -3,6 +3,7 @@ package com.skillone.designpatterns.adapter.classadapter;
 public class Client {
 
     public static void main(String[] args) {
+
         Phone phone = new Phone();
         phone.charging(new VoltageAdapter());
     }

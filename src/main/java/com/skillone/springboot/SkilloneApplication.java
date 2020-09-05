be891a8e158fb0328139cd20e71be17e67723933
jacky_gy_zhu <jacky.gy.zhu@gmail.com>
2020-09-05 13:29:09 +1000

    first change

--- src/main/java/com/skillone/springboot/SkilloneApplication.java
+++ src/main/java/com/skillone/springboot/SkilloneApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SkilloneApplication {
 
     public static void main(String[] args) {
+
+        // run application
         SpringApplication.run(SkilloneApplication.class, args);
     }
 

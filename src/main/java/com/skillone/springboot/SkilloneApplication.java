8e3ac29dbfcb4f6f8244a81b544b87516232ab3a
jacky_gy_zhu <jacky.gy.zhu@gmail.com>
2020-09-05 13:24:12 +1000

    init

---
+++ src/main/java/com/skillone/springboot/SkilloneApplication.java
@@ -0,0 +1,13 @@
+package com.skillone.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkilloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkilloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skillone/springboot/SkilloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillone.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkilloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

43aa6e7decc8f45dc82692cf1511f8e23f3be761
jacky-gy-zhu <jacky.gy.zhu@gmail.com>
2020-10-16 19:33:09 +1100

    update

--- react/web/webpack.config.js
+++ react/web/webpack.config.js
@@ -1,51 +1,95 @@
 const {resolve} = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+const webpack = require('webpack')
+const AddAssetHtmlWebpackPlugin = require('add-asset-html-webpack-plugin')
+const PUBLIC_PATH = '/'
+
+process.env.NODE_ENV = "production"
+
+const commonCssLoader = [
+    // usYloader取代style-loader，作用：提取js中的css成单独文件
+    MiniCssExtractPlugin.loader,
+    'css-loader',
+    {
+        loader: 'postcss-loader',
+        options: {
+            postcssOptions: {
+                plugins: [
+                    [
+                        'postcss-preset-env',
+                        {
+                            // Options
+                        },
+                    ],
+                ],
+            },
+        },
+    }
+]
 
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'chunk.js',
-        path: resolve(__dirname, 'build')
+        filename: 'js/[name].[contenthash:10].js',
+        path: resolve(__dirname, 'build'),
+        publicPath: PUBLIC_PATH
     },
     module: {
         rules: [
             {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                options: {
-                    "presets": [
-                        "@babel/preset-env",
-                        "@babel/preset-react",
-                        {
-                            "plugins": ['@babel/plugin-proposal-class-properties']
-                        }
-                    ]
-                }
-            },
-            {
-                test: /\.css$/,
-                exclude: /node_modules/,
-                use: [
-                    { loader: 'style-loader' },
+                oneOf: [
                     {
-                        loader: 'css-loader',
+                        test: /\.css$/,
+                        use: [...commonCssLoader]
+                    },
+                    {
+                        test: /\.less$/,
+                        use: [
+                            ...commonCssLoader,
+                            'less-loader'
+                        ]
+                    },
+                    {
+                        test: /\.(js|jsx)$/,
+                        exclude: /node_modules/,
+                        loader: 'babel-loader',
                         options: {
-                            modules: {
-                                localIdentName: "[name]__[local]___[hash:base64:5]",
-                            },
-                            sourceMap: true
+                            "presets": [
+                                "@babel/preset-env",
+                                "@babel/preset-react",
+                                {
+                                    "plugins": ['@babel/plugin-proposal-class-properties']
+                                }
+                            ]
                         }
                     },
                     {
-                        loader: 'postcss-loader',
-                        options: {
-                            postcssOptions: {
-                                plugins: [
-                                    [ 'autoprefixer', {}, ],
-                                ],
+                        test: /\.css$/,
+                        exclude: /node_modules/,
+                        use: [
+                            { loader: 'style-loader' },
+                            {
+                                loader: 'css-loader',
+                                options: {
+                                    modules: {
+                                        localIdentName: "[name]__[local]___[hash:base64:5]",
+                                    },
+                                    sourceMap: true
+                                }
                             },
-                        }
+                            {
+                                loader: 'postcss-loader',
+                                options: {
+                                    postcssOptions: {
+                                        plugins: [
+                                            [ 'autoprefixer', {}, ],
+                                        ],
+                                    },
+                                }
+                            }
+                        ]
                     }
                 ]
             }

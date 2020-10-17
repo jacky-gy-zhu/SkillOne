f5a48b6cf05d1ef08a6f200ac62dc7c7e2854315
jacky-gy-zhu <jacky.gy.zhu@gmail.com>
2020-10-17 14:29:52 +1100

    update

--- react/web/src/index.js
+++ react/web/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {render} from 'react-dom'
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter, HashRouter} from 'react-router-dom'
 import {Provider} from 'react-redux'
 
 import store from './redux/store'
@@ -10,9 +10,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 render(
     (
         <Provider store={store}>
-            <BrowserRouter>
+            <HashRouter>
                 <App/>
-            </BrowserRouter>
+            </HashRouter>
         </Provider>
     ),
     document.getElementById('root')
